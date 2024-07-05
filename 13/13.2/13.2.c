5c1cdf707e345a76b44b788680d2f03a38addff2
Наталья <154578644+BalNatali@users.noreply.github.com>
2024-07-05 20:04:30 +0300

    Add files via upload

---
+++ 13/13.2/13.2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define PORT 8080
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+    char socket_name[100];
+
+    printf("Enter the socket name: ");
+    fgets(socket_name, 100, stdin);
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation error");
+        exit(EXIT_FAILURE);
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_post = htons(PORT);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        perror("Invalid address/ Address not supported");
+        exit(EXIT_FAILURE);
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connection Failed");
+        exit(EXIT_FAILURE);
+    }
+
+    send(sock, socket_name, strlen(socket_name), 0);
+    printf("Socket name sent");
+
+    close(sock);
+
+    return 0;
+}
